65b82b2df3f67360bc0c70dc49150737798503e2
TheCodeSummoner <kacper.florianski@gmail.com>
2021-02-02 17:39:03 +0000

    Fixed file paths

--- assets/scripts/launch-vegas.sh
+++ assets/scripts/launch-vegas.sh
@@ -2,4 +2,4 @@ export WORKSPACE=${WORKSPACE:-/root/workspace}
 
 # shellcheck disable=SC1090
 source "$WORKSPACE"/setup-workspace.sh
-roslaunch f1tenth_simulator simulator.launch map:="$WORKSPACE"/code/f1tenth-racing-algorithms/maps/vegas.yaml
+roslaunch f1tenth_simulator simulator.launch map:="$WORKSPACE"/code/f1tenth-racing-algorithms/assets/maps/vegas.yaml
--- assets/scripts/launch-waypoints-follower.sh
+++ assets/scripts/launch-waypoints-follower.sh
@@ -2,4 +2,4 @@ export WORKSPACE=${WORKSPACE:-/root/workspace}
 
 # shellcheck disable=SC1090
 source "$WORKSPACE"/setup-workspace.sh
-python3 "$WORKSPACE"/code/f1tenth-racing-algorithms/distance_mpc
+python3 "$WORKSPACE"/code/f1tenth-racing-algorithms/f1tenth/waypoints_follower
