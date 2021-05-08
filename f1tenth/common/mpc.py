17cd08484a7dafaa5b550f39bb02a9d646c1af3b
TheCodeSummoner <kacper.florianski@gmail.com>
2021-05-08 12:08:55 +0200

    ...

--- f1tenth/common/mpc.py
+++ f1tenth/common/mpc.py
@@ -345,7 +345,7 @@ class PointFollowerMPC(ModelPredictiveControl):
         """
         No stage cost is specified in this approach.
         """
-        return 10 - self._velocity
+        return 7 - self._velocity
 
     @property
     def terminal_cost(self):
@@ -357,7 +357,7 @@ class PointFollowerMPC(ModelPredictiveControl):
         #psi_ref = casadi.atan2(self._target_y - self._position_y, self._target_x - self._position_x)
         #ha_cost = casadi.atan2(casadi.sin(self._heading_angle - psi_ref), casadi.cos(self._heading_angle - psi_ref))
         return ((self._target_x - self._position_x) ** 2 + (self._target_y - self._position_y) ** 2) \
-            * (self._heading_angle - casadi.atan2(self._target_y - self._position_y, self._target_x - self._position_x)) ** 2
+            + (self._heading_angle - casadi.atan2(self._target_y - self._position_y, self._target_x - self._position_x)) ** 2
 
     def configure_model(self):
         """
