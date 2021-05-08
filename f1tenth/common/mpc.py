2de3f00ea6ce3e23fce50de8c58a2c390d76880e
TheCodeSummoner <kacper.florianski@gmail.com>
2021-05-08 14:15:28 +0200

    ...

--- f1tenth/common/mpc.py
+++ f1tenth/common/mpc.py
@@ -364,7 +364,7 @@ class PointFollowerMPC(ModelPredictiveControl):
         ha_cost = (self._heading_angle - casadi.fmin(casadi.atan2(self._target_y - self._position_y, self._target_x - self._position_x) ** 2, 0.001))
         d_cost = (self._target_x - self._position_x) ** 2 + (self._target_y - self._position_y) ** 2
         d_cost_base = (self._target_x - self._sx) ** 2 + (self._target_y - self._sy) ** 2
-        return d_cost + ha_cost
+        return 1000* d_cost + ha_cost
 
     def configure_model(self):
         """
