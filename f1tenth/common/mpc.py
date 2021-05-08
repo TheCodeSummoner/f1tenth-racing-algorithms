48d10aee62c7a6a6114e9da062bd7d7f0bd36730
TheCodeSummoner <kacper.florianski@gmail.com>
2021-05-08 12:34:11 +0200

    ...

--- f1tenth/common/mpc.py
+++ f1tenth/common/mpc.py
@@ -179,6 +179,7 @@ class ModelPredictiveControl(ABC):
             "ipopt.print_level": 0,
             "ipopt.sb": "yes",
             "ipopt.max_iter": 300,
+            'ipopt.linear_solver': 'MA27',
             "print_time": 0
         } if self._suppress_outputs else {
 
