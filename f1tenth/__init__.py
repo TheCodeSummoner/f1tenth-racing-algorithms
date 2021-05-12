ed1aa5716e8bcee992cf1f7670c13472bacc6304
TheCodeSummoner <kacper.florianski@gmail.com>
2021-05-12 16:50:01 +0200

    Don't import packages directly, to allow clearer README instructions

--- f1tenth/__init__.py
+++ f1tenth/__init__.py
@@ -1,16 +1,3 @@
 """
 Collection of F1TENTH-compatible autonomous control algorithms.
 """
-from . import reference_ftg
-from . import waypoints_follower
-from . import halves_ftg
-from . import middle_point_ftg
-from . import farthest_point_ftg
-
-__all__ = [
-    "reference_ftg",
-    "waypoints_follower",
-    "halves_ftg",
-    "middle_point_ftg",
-    "farthest_point_ftg",
-]
